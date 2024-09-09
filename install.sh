ea60c69f20905bab2aab1e826b99a03a0c869092
ElectricCowBoy123 <johnfearn7@gmail.com>
2024-09-09 23:04:15 +0100

    hungarian notation

--- install.sh
+++ install.sh
@@ -24,16 +24,16 @@
 #  ██  ██  ██   ██ ██   ██ ██ ██   ██ ██   ██ ██      ██           ██ 
 #   ████   ██   ██ ██   ██ ██ ██   ██ ██████  ███████ ███████ ███████ 
                                                                                                  
-export c_reset="\033[0m"
-export c_blue="\033[1;34m"
-export c_magenta="\033[1;35m"
-export c_cyan="\033[1;36m"
-export c_green="\033[1;32m"
-export c_red="\033[1;31m"
-export c_yellow="\033[1;33m"   
-export c_italic='\033[3m'                                         
+export RESET="\033[0m";readonly RESET;
+export BLUE="\033[1;34m";readonly BLUE;
+export MAGENTA="\033[1;35m";readonly MAGENTA;
+export CYAN="\033[1;36m";readonly CYAN;
+export GREEN="\033[1;32m";readonly GREEN;
+export RED="\033[1;31m";readonly RED;
+export YELLOW="\033[1;33m";readonly YELLOW;
+export ITALIC="\033[3m";readonly ITALIC;                                   
 
-required_packages=("jq" "sed")
+arrStr_required_packages=("jq" "sed")
 readonly REPO_DIR="$(dirname "$(readlink -m "${0}")")"
 readonly JSON_DIR="${REPO_DIR}/conf/install.json"
 source "${REPO_DIR}/lib/core.sh"
@@ -43,15 +43,14 @@ source "${REPO_DIR}/lib/core.sh"
 # ███████ ██████  ██   ███ ██    ██ ██ ████ ██ █████   ██ ██  ██    ██        ██████  ███████ ██████  ███████ ██ ██ ██  ██ ██   ███ 
 # ██   ██ ██   ██ ██    ██ ██    ██ ██  ██  ██ ██      ██  ██ ██    ██        ██      ██   ██ ██   ██      ██ ██ ██  ██ ██ ██    ██ 
 # ██   ██ ██   ██  ██████   ██████  ██      ██ ███████ ██   ████    ██        ██      ██   ██ ██   ██ ███████ ██ ██   ████  ██████  
-                                                                                                                                  
-                                                                                                                                  
+                                                                                       
 if [ "$EUID" -ne 0 ]; then
-  printf "${c_italic}${c_red}%s\n${c_reset}" "[Error]: Please Run as Root!"
+  printf "${ITALIC}${RED}%s\n${RESET}" "[Error]: Please Run as Root!"
   exit 1
 fi
 
 if [ $# -eq 0 ]; then
-    printf "${c_magenta}%s\n${c_reset}" "
+    printf "${MAGENTA}%s\n${RESET}" "
  ██████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
  ██                                                                                                                      ██   
  ██   ███████ ██      ███████  ██████ ████████ ██████  ██  ██████  ██████  ██████  ██     ██ ██████   ██████  ██    ██   ██
@@ -69,7 +68,7 @@ if [ $# -eq 0 ]; then
  ██                                                                                                                      ██
  ██████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████  
 "
-    printf "\n${c_italic}${c_yellow}%s\n${c_reset}" "No Arguments Supplied, Returning Help..."
+    printf "\n${ITALIC}${YELLOW}%s\n${RESET}" "No Arguments Supplied, Returning Help..."
     help
     exit 0
 fi
@@ -85,35 +84,35 @@ while [[ $# -gt 0 ]]; do
             ;;
         -i|--install) 
             # todo: implement subargments to install gaming packages etc
-            check_required_packages "${required_packages[@]}"
+            check_required_packages "${arrStr_required_packages[@]}"
             check_package_managers
             parse_json
-            install_packages "$packages"
+            install_packages "$str_packages"
             install_nvidia_drivers
             clean_up
             shift
             ;;
         -g|--gits)
             parse_json
-            handle_gits "${gits[@]}"
+            handle_gits "${arrStr_gits[@]}"
             clean_up
             shift
             ;;
         -s|--settings)
-            printf "${c_italic}${c_yellow}%s\n\n${c_reset}" "you did s"
+            printf "${ITALIC}${YELLOW}%s\n\n${RESET}" "you did s"
             clean_up
             shift
             ;;
         -f|--flatpaks)
-            required_packages+=("flatpak")
-            check_required_packages "${required_packages[@]}"
+            arrStr_required_packages+=("flatpak")
+            check_required_packages "${arrStr_required_packages[@]}"
             parse_json
-            install_flatpaks "${flatpaks[@]}"
+            install_flatpaks "${arrStr_flatpaks[@]}"
             clean_up
             shift
             ;;
         *)
-            printf "${c_italic}${c_red}%s\n${c_reset}" "[Error]: Unknown argument! '$1'"
+            printf "${ITALIC}${RED}%s\n${RESET}" "[Error]: Unknown argument! '$1'"
             exit 0
             ;;
     esac
--- lib/core.sh
+++ lib/core.sh
@@ -15,118 +15,123 @@ help(){
 }
 
 help_entry() {
-  printf "${c_blue}%s ${c_italic}${c_cyan}%s${c_magenta}%s ${c_green}%s\n${c_reset}" "${1}" "${2}" "${3}" "${4}"
+  printf "${BLUE}%s ${ITALIC}${CYAN}%s${MAGENTA}%s ${GREEN}%s\n${RESET}" "${1}" "${2}" "${3}" "${4}"
 }
 
 check_package_managers() {
     # Ensure these are organized in order of importance as first item in array will be primary method of installing packages for the run
-    pacman=()
+    arrStr_pacman=()
     
     if command -v dnf &> /dev/null; then
-        pacman+=("dnf")
+        arrStr_pacman+=("dnf")
     fi
 
     if command -v yum &> /dev/null; then
-        pacman+=("yum")
+        arrStr_pacman+=("yum")
     fi
 
     if command -v apt &> /dev/null; then
-        pacman+=("apt")
+        arrStr_pacman+=("apt")
     fi
 
     if command -v apt-get &> /dev/null; then
-        pacman+=("apt-get")
+        arrStr_pacman+=("apt-get")
     fi
 
     if command -v pacman &> /dev/null; then
-        pacman+=("pacman")
+        arrStr_pacman+=("pacman")
     fi
 
     if command -v zypper &> /dev/null; then
-        pacman+=("zypper")
+        arrStr_pacman+=("zypper")
     fi
 
     if command -v rpm &> /dev/null; then
-        pacman+=("rpm")
+        arrStr_pacman+=("rpm")
     fi
 
     if command -v dpkg &> /dev/null; then
-        pacman+=("dpkg")
+        arrStr_pacman+=("dpkg")
     fi
 
     if command -v snap &> /dev/null; then
-        pacman+=("snap")
+        arrStr_pacman+=("snap")
     fi
 
     if command -v emerge &> /dev/null; then
-        pacman+=("portage")
+        arrStr_pacman+=("portage")
     fi
 
     if command -v flatpak &> /dev/null; then
-        pacman+=("flatpak")
+        arrStr_pacman+=("flatpak")
     fi
 
-    if [ ${#pacman[@]} -eq 0 ]; then
-        pacman+=("error")
+    if [ ${#arrStr_pacman[@]} -eq 0 ]; then
+        arrStr_pacman+=("error")
     fi
 }
 
 check_required_packages(){
     if [[ $# -eq 0 ]]; then
-        printf "${c_italic}${c_red}%s\n${c_reset}\n" "[Error]: No arguments supplied in check_required_packages function!"
+        printf "${ITALIC}${RED}%s\n${RESET}\n" "[Error]: No arguments supplied in check_required_packages function!"
         return 1
     fi
 
-    local packages=("$@")
+    local str_packages=("$@")
     
-    for package in "${packages[@]}"; do
-        if [[ ! -n $(command -v "$package") ]]; then
-            printf "${c_red}%s is not Installed.\n\n${c_reset}" "$package"
+    for str_package in "${str_packages[@]}"; do
+        if [[ ! -n $(command -v "$str_package") ]]; then
+            printf "${RED}%s is not Installed.\n\n${RESET}" "$str_package"
             exit 1
         fi
     done
 }
 
+validate_json(){
+    #TODO
+    echo "awdawd"
+}
+
 parse_json(){
-    while IFS= read -r line; do
-        gits+=("$line")
+    while IFS= read -r str_line; do
+        arrStr_gits+=("$str_line")
     done < <(jq -r ".git[]" "$JSON_DIR")
 
-    while IFS= read -r line; do
-        flatpaks+=("$line")
+    while IFS= read -r str_line; do
+        arrStr_flatpaks+=("$str_line")
     done < <(jq -r ".flatpaks[]" "$JSON_DIR")
 
-    nvidia_drivers=$(jq -r '.["nvidia-drivers"]' "$JSON_DIR")
+    str_nvidia_drivers=$(jq -r '.["nvidia-drivers"]' "$JSON_DIR")
 
-    if [[ ${pacman[0]} == "dnf" ]]; then
-        packages=""
-        elements=$(jq -r ".packages.${pacman[0]}[] | .[]" "$JSON_DIR")
-        while IFS= read -r element; do
-            packages+="$element "
-        done <<< "$elements"
+    if [[ ${arrStr_pacman[0]} == "dnf" ]]; then
+        str_packages=""
+        str_elements=$(jq -r ".packages.${arrStr_pacman[0]}[] | .[]" "$JSON_DIR")
+        while IFS= read -r str_element; do
+            str_packages+="$str_element "
+        done <<< "$str_elements"
     fi
 
-    if [[ ${pacman[0]} == "apt-get" ]]; then
-        packages=""
-        elements=$(jq -r ".packages.${pacman[0]}[] | .[]" "$JSON_DIR")
-        while IFS= read -r element; do
-            packages+="$element "
-        done <<< "$elements"
+    if [[ ${arrStr_pacman[0]} == "apt-get" ]]; then
+        str_packages=""
+        str_elements=$(jq -r ".packages.${arrStr_pacman[0]}[] | .[]" "$JSON_DIR")
+        while IFS= read -r str_element; do
+            str_packages+="$str_element "
+        done <<< "$str_elements"
     fi
 
-    if [[ ${pacman[0]} == "apt" ]]; then
-        packages=""
-        elements=$(jq -r ".packages.${pacman[0]}[] | .[]" "$JSON_DIR")
-        while IFS= read -r element; do
-            packages+="$element "
-        done <<< "$elements"
+    if [[ ${arrStr_pacman[0]} == "apt" ]]; then
+        str_packages=""
+        str_elements=$(jq -r ".packages.${arrStr_pacman[0]}[] | .[]" "$JSON_DIR")
+        while IFS= read -r str_element; do
+            str_packages+="$str_element "
+        done <<< "$str_elements"
     fi
 }
 
 clean_up(){
-    if [[ ${pacman[0]} == "dnf" ]]; then
-        printf "\n${c_green}%s\n${c_reset}\n" "Clearing dnf Cache..."
-        dnf clean all || { printf "${c_italic}${c_red}%s\n${c_reset}\n" "[Error]: Failed to Clean up dnf Cache! '$1'"; exit 1; }
+    if [[ ${arrStr_pacman[0]} == "dnf" ]]; then
+        printf "\n${GREEN}%s\n${RESET}\n" "Clearing dnf Cache..."
+        dnf clean all || { printf "${ITALIC}${RED}%s\n${RESET}\n" "[Error]: Failed to Clean up dnf Cache! '$1'"; exit 1; }
         exit 0
     fi
 }
@@ -139,107 +144,106 @@ clean_up(){
                                                                                                                                                                                                                                                                                                                                                      
 install_packages(){
     if [[ $# -eq 0 ]]; then
-        printf "${c_italic}${c_red}%s\n${c_reset}\n" "[Error]: No arguments supplied in install_package function!"
+        printf "${ITALIC}${RED}%s\n${RESET}\n" "[Error]: No arguments supplied in install_package function!"
         return 1
     fi
 
-    for element in "${pacman[@]}"; do
-        if [[ $element == "error" ]]; then
-            printf "${c_italic}${c_red}%s\n${c_reset}\n" "[Error]: No recognized package managers were found on your system, unable to execute script!"
+    for str_element in "${arrStr_pacman[@]}"; do
+        if [[ $str_element == "error" ]]; then
+            printf "${ITALIC}${RED}%s\n${RESET}\n" "[Error]: No recognized package managers were found on your system, unable to execute script!"
             return 1
         fi
     done
 
     if [ $# -eq 0 ] || [ -z "$1" ]; then
-        printf "${c_italic}${c_red}%s\n${c_reset}\n" "[Error]: No packages supplied!"
+        printf "${ITALIC}${RED}%s\n${RESET}\n" "[Error]: No packages supplied!"
         return 1
     fi
 
-    if [[ ${pacman[0]} == "dnf" ]]; then
-        printf "${c_green}%s\n${c_reset}\n" "Updating Package Repositories..."
-        dnf update --refresh -y || { printf "${c_italic}${c_red}%s\n${c_reset}\n" "[Error]: Failed to dnf update!"; exit 1; }
+    if [[ ${arrStr_pacman[0]} == "dnf" ]]; then
+        printf "${GREEN}%s\n${RESET}\n" "Updating Package Repositories..."
+        dnf update --refresh -y || { printf "${ITALIC}${RED}%s\n${RESET}\n" "[Error]: Failed to dnf update!"; exit 1; }
 
-        printf "\n${c_green}%s\n${c_reset}\n" "Installing Package(s) '$1'..."
-        dnf install -y ${1} || { printf "${c_italic}${c_red}%s\n${c_reset}\n" "[Error]: Failed to Install Package(s)! '$1'"; exit 1; }
+        printf "\n${GREEN}%s\n${RESET}\n" "Installing Package(s) '$1'..."
+        dnf install -y ${1} || { printf "${ITALIC}${RED}%s\n${RESET}\n" "[Error]: Failed to Install Package(s)! '$1'"; exit 1; }
 
         return 0
     fi
 
-    if [[ ${pacman[0]} == "apt" ]]; then
-        printf "${c_green}%s\n${c_reset}\n" "Updating Package Repositories..."
-        apt update || { printf "${c_italic}${c_red}%s\n${c_reset}\n" "[Error]: Failed to apt update!"; exit 1; }
+    if [[ ${arrStr_pacman[0]} == "apt" ]]; then
+        printf "${GREEN}%s\n${RESET}\n" "Updating Package Repositories..."
+        apt update || { printf "${ITALIC}${RED}%s\n${RESET}\n" "[Error]: Failed to apt update!"; exit 1; }
 
-        printf "\n${c_green}%s\n${c_reset}\n" "Installing Package(s) '$1'..."
-        apt install "${1}" || { printf "${c_italic}${c_red}%s\n${c_reset}\n" "[Error]: Failed to Install Package(s)! '$1'"; exit 1; }
+        printf "\n${GREEN}%s\n${RESET}\n" "Installing Package(s) '$1'..."
+        apt install "${1}" || { printf "${ITALIC}${RED}%s\n${RESET}\n" "[Error]: Failed to Install Package(s)! '$1'"; exit 1; }
         return 0
     fi
 
-    if [[ ${pacman[0]} == "apt-get" ]]; then
-        printf "${c_green}%s\n${c_reset}\n" "Updating Package Repositories..."
-        apt-get update || { printf "${c_italic}${c_red}%s\n${c_reset}\n" "[Error]: Failed to apt-get update!"; exit 1; }
+    if [[ ${arrStr_pacman[0]} == "apt-get" ]]; then
+        printf "${GREEN}%s\n${RESET}\n" "Updating Package Repositories..."
+        apt-get update || { printf "${ITALIC}${RED}%s\n${RESET}\n" "[Error]: Failed to apt-get update!"; exit 1; }
 
-        printf "\n${c_green}%s\n${c_reset}\n" "Installing Package(s) '$1'..."
-        apt-get install "${1}" || { printf "${c_italic}${c_red}%s\n${c_reset}\n" "[Error]: Failed to Install Package(s)! '$1'"; exit 1; }
+        printf "\n${GREEN}%s\n${RESET}\n" "Installing Package(s) '$1'..."
+        apt-get install "${1}" || { printf "${ITALIC}${RED}%s\n${RESET}\n" "[Error]: Failed to Install Package(s)! '$1'"; exit 1; }
         return 0
     fi
 }
 
 handle_gits(){
     if [[ $# -eq 0 ]]; then
-        printf "${c_italic}${c_red}%s\n${c_reset}\n" "[Error]: No arguments supplied in handle_gits function!"
+        printf "${ITALIC}${RED}%s\n${RESET}\n" "[Error]: No arguments supplied in handle_gits function!"
         return 1
     fi
 
     if [ $# -eq 0 ] || [ -z "$1" ]; then
-        printf "${c_italic}${c_red}%s\n${c_reset}\n" "[Error]: No gits supplied!"
+        printf "${ITALIC}${RED}%s\n${RESET}\n" "[Error]: No gits supplied!"
         return 1
     fi
 
-    for git in "${gits[@]}"; do
-
-        if [[ $git == "https://github.com/vinceliuice/WhiteSur-gtk-theme.git" ]]; then
+    for str_git in "${arrStr_gits[@]}"; do
+        if [[ $str_git == "https://github.com/vinceliuice/WhiteSur-gtk-theme.git" ]]; then
             if [[ -f "/home/$SUDO_USER/.themes/WhiteSur-Dark" && -f "/home/$SUDO_USER/.themes/WhiteSur-Light" ]]; then
-                printf  "\n${c_green}%s\n${c_reset}\n" "Installing 'WhiteSur-gtk-theme'..."
-                git clone --depth=1 "$git" "/home/$SUDO_USER/Downloads/WhiteSur-gtk-theme"
+                printf  "\n${GREEN}%s\n${RESET}\n" "Installing 'WhiteSur-gtk-theme'..."
+                git clone --depth=1 "$str_git" "/home/$SUDO_USER/Downloads/WhiteSur-gtk-theme"
                 chmod +x "/home/$SUDO_USER/Downloads/WhiteSur-gtk-theme/install.sh"
             else
-                printf "\n${c_green}%s\n${c_reset}\n" "'WhiteSur-gtk-theme' Already Installed."
+                printf "\n${GREEN}%s\n${RESET}\n" "'WhiteSur-gtk-theme' Already Installed."
             fi
         fi
 
-        if [[ $git == "https://github.com/ohmyzsh/ohmyzsh.git" ]]; then
+        if [[ $str_git == "https://github.com/ohmyzsh/ohmyzsh.git" ]]; then
             if [[ -f "/home/$SUDO_USER/.oh-my-zsh" ]]; then
-                printf  "\n${c_green}%s\n${c_reset}\n" "Installing 'ohmyzsh'..."
-                git clone "$git" "/home/$SUDO_USER/Downloads/ohmyzsh"
+                printf  "\n${GREEN}%s\n${RESET}\n" "Installing 'ohmyzsh'..."
+                git clone "$str_git" "/home/$SUDO_USER/Downloads/ohmyzsh"
                 chmod +x "/home/$SUDO_USER/Downloads/ohmyzsh/tools/install.sh"
-                sh "/home/$SUDO_USER/Downloads/ohmyzsh/tools/install.sh" || { printf "${c_italic}${c_red}%s\n${c_reset}\n" "[Error]: Error occurred installing 'ohmyzsh'! '$1'"; exit 1; }
+                sh "/home/$SUDO_USER/Downloads/ohmyzsh/tools/install.sh" || { printf "${ITALIC}${RED}%s\n${RESET}\n" "[Error]: Error occurred installing 'ohmyzsh'! '$1'"; exit 1; }
             else
-                printf "\n${c_green}%s\n${c_reset}\n" "'ohmyzsh' Already Installed."
+                printf "\n${GREEN}%s\n${RESET}\n" "'ohmyzsh' Already Installed."
             fi
         fi
 
-        if [[ $git == "https://github.com/romkatv/powerlevel10k.git" ]]; then
+        if [[ $str_git == "https://github.com/romkatv/powerlevel10k.git" ]]; then
             if [[ ! $(grep -Fxq "source /home/$SUDO_USER/.powerlevel10k/powerlevel10k.zsh-theme" /home/$SUDO_USER/.zshrc; echo $?) -ne 0 ]]; then
-                printf "\n${c_green}%s\n${c_reset}\n" "Installing 'powerlevel10k' Recommended Font..."
+                printf "\n${GREEN}%s\n${RESET}\n" "Installing 'powerlevel10k' Recommended Font..."
                 curl -L -o "/home/$SUDO_USER/Downloads/MesloLGS_NF_Regular.ttf" 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf'
                 mv "/home/$SUDO_USER/Downloads/MesloLGS_NF_Regular.ttf" "/home/$SUDO_USER/.local/share/fonts/"
                 fc-cache -fv
 
-                printf "\n${c_green}%s\n${c_reset}\n" "Installing 'powerlevel10k'..."
-                git clone --depth=1 "https://github.com/romkatv/powerlevel10k.git" "/home/$SUDO_USER/.powerlevel10k"
+                printf "\n${GREEN}%s\n${RESET}\n" "Installing 'powerlevel10k'..."
+                git clone --depth=1 "$str_git" "/home/$SUDO_USER/.powerlevel10k"
                 echo 'source /home/$SUDO_USER/.powerlevel10k/powerlevel10k.zsh-theme' >>/home/$SUDO_USER/.zshrc
             else
-                printf "\n${c_green}%s\n${c_reset}\n" "'powerlevel10k' Already Installed."
+                printf "\n${GREEN}%s\n${RESET}\n" "'powerlevel10k' Already Installed."
             fi
         fi
         
-        if [[ $git == "https://github.com/vinceliuice/McMojave-circle.git" ]]; then
+        if [[ $str_git == "https://github.com/vinceliuice/McMojave-circle.git" ]]; then
             if [[ -f "/home/$SUDO_USER/.local/share/icons/McMojave-circle" && -f "/home/$SUDO_USER/.local/share/icons/McMojave-circle-dark" && -f "/home/$SUDO_USER/.local/share/icons/McMojave-circle-light" ]]; then
-                printf "\n${c_green}%s\n${c_reset}\n" "Installing 'McMojave-circle'..."
-                git clone "$git" "/home/$SUDO_USER/Downloads/McMojave-circle"
+                printf "\n${GREEN}%s\n${RESET}\n" "Installing 'McMojave-circle'..."
+                git clone "$str_git" "/home/$SUDO_USER/Downloads/McMojave-circle"
                 # TODO
             else
-                printf "\n${c_green}%s\n${c_reset}\n" "'McMojave-circle' Already Installed."
+                printf "\n${GREEN}%s\n${RESET}\n" "'McMojave-circle' Already Installed."
             fi
         fi
     done
@@ -253,27 +257,27 @@ apply_settings(){
 }
 
 install_flatpaks(){
-    printf "\n${c_green}%s\n${c_reset}\n" "Installing Flatpaks..."
-    flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo || { "${c_italic}${c_red}%s\n${c_reset}\n" "[Error]: Failed to Add Flathub Repo to Flatpak!"; exit 1;}
-    for flatpak in "${flatpaks[@]}"; do
-        flatpak install -y flathub $flatpak
+    printf "\n${GREEN}%s\n${RESET}\n" "Installing Flatpaks..."
+    flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo || { "${ITALIC}${RED}%s\n${RESET}\n" "[Error]: Failed to Add Flathub Repo to Flatpak!"; exit 1;}
+    for str_flatpak in "${arrStr_flatpaks[@]}"; do
+        flatpak install -y flathub $str_flatpak
     done
 }
 
 install_nvidia_drivers(){
     if command -v nvidia-smi &> /dev/null; then
-        printf "\n${c_green}%s\n${c_reset}\n" "Nvidia Drivers are Already Installed, Skipping."
+        printf "\n${GREEN}%s\n${RESET}\n" "Nvidia Drivers are Already Installed, Skipping."
         return 1
     fi
     
-    if [[ $nvidia_drivers == "true" ]]; then
+    if [[ $str_nvidia_drivers == "true" ]]; then
         if lspci | grep -i nvidia &> /dev/null; then
-            printf "\n${c_green}%s\n${c_reset}\n" "Installing Nvidia Drivers..."
-            if [[ ${pacman[0]} == "dnf" ]]; then
+            printf "\n${GREEN}%s\n${RESET}\n" "Installing Nvidia Drivers..."
+            if [[ ${arrStr_pacman[0]} == "dnf" ]]; then
                 dnf install -y kernel-devel kernel-headers gcc make dkms acpid libglvnd-opengl libglvnd-devel pkgconfig
                 dnf install -y "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
                 dnf install -y "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
-                dnf makecache || { "${c_italic}${c_red}%s\n${c_reset}\n" "[Error]: Failed to Update the dnf Metadata Cache!"; exit 1;}
+                dnf makecache || { "${ITALIC}${RED}%s\n${RESET}\n" "[Error]: Failed to Update the dnf Metadata Cache!"; exit 1;}
                 dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda
             fi
         fi
